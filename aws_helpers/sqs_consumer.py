78a746ad5641eb427f10bf8ea9f07e52788ec3d8
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-02-21 10:46:01 -0500

    adding library class that will provide functionality to consume from SQS Queues in AWS. currenly stub

---
+++ aws_helpers/sqs_consumer.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        SQS Consumer Class. Will provide functionality to
+        consume from SQS Queues in AWS
+    Examples of Create Object of Class:
+        sqs_consumer_obj = SQSConsumer()
+"""
+
+# Python Library Imports
+import logging
+import wrapt
+
+
+class SQSConsumer(object):
+    """
+        SQSConsumer Class
+    """
+
+    ###
+    # Class Lifecycle Methods
+    ###
+
+    def __init__(self):
+        """
+        Purpose:
+            Initilize the SQSConsumer Class.
+        Args:
+            N/A
+        """
+
+        pass
+
+    def __del__(self):
+        """
+        Purpose:
+            Delete the SQSConsumer Object.
+        Args:
+            N/A
+        """
+
+        pass
